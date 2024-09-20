b12845353509e146c1da8aa239d1f2daf9dcd97b
Brenno Alencar <127119853+BrennoAlenkar@users.noreply.github.com>
2024-09-19 22:24:46 -0300

    Atualizei

---
+++ assets/js/scrol.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const cards = document.querySelectorAll('.custom-card');
+
+    const options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.1 // 10% do cartão visível
+    };
+
+    const callback = (entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.parentNode.classList.add('visible'); // Adiciona a classe ao pai (card-link)
+            }
+        });
+    };
+
+    const observer = new IntersectionObserver(callback, options);
+
+    cards.forEach(card => {
+        observer.observe(card);
+    });
+});
